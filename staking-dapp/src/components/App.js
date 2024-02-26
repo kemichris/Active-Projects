40ecc9d897a42b80bee2bf9ab3259ed6543fa1ec
kemi Chris <ekeminichristopher123@gmail.com>
2024-02-26 12:12:11 +0100

    new

---
+++ staking-dapp/src/components/App.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react'
+
+import Navbar from './Navbar';
+import Main from "./Main"
+import Web3 from 'web3';
+
+import Tether from "../truffle_abis/Tether.json";
+import RWD from "../truffle_abis/RWD.json";
+import DecentralBank from "../truffle_abis/DecentralBank.json";
+
+class App extends Component {
+
+  async UNSAFE_componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = new Web3.apply(window.web3.currentProvider)
+    } else {
+      window.alert("No evm brower detected! check out MetaMask!")
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const account = await web3.eth.getAccounts()
+    this.setState({ account: account[0] })
+    const networkId = await web3.eth.net.getId()
+
+
+    //load tether contract
+
+    const tetherData = Tether.networks[networkId]
+
+    if (tetherData) {
+      const tether = new web3.eth.Contract(Tether.abi, tetherData.address)
+      this.setState({ tether })
+
+      let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
+      this.setState({ tetherbalance: tetherBalance.toString() })
+
+      console.log({ tetherbalance: tetherBalance })
+
+
+    } else {
+      window.alert("Error! Tether contract not deployed - no detected netork")
+    }
+
+    //load RWD contract
+    const rwdData = RWD.networks[networkId]
+
+    if (rwdData) {
+      const rwd = new web3.eth.Contract(RWD.abi, rwdData.address)
+      this.setState({ rwd })
+
+      let rwdBalance = await rwd.methods.balanceOf(this.state.account).call()
+      this.setState({ rwdBalance: rwdBalance.toString() })
+
+      console.log({ rwdBalance: rwdBalance })
+
+    } else {
+      window.alert("Error! Reward token not deployed - no detected netork")
+    }
+
+
+    //load Decentral bank contract
+    const decentralBankData = DecentralBank.networks[networkId]
+
+    if (decentralBankData) {
+      const decentralBank = new web3.eth.Contract(DecentralBank.abi, decentralBankData.address)
+      this.setState({ decentralBank })
+
+      let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call()
+      this.setState({ stakingBalance: stakingBalance.toString() })
+
+      console.log({ stakingBalance: stakingBalance })
+
+    } else {
+      window.alert("Error! Decentral bank not deployed - no detected netork")
+    }
+
+    this.setState({ loading: false })
+
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: "0x0",
+      tether: {},
+      rwd: {},
+      decentralBank: {},
+      tetherBalance: {},
+      rwdBalance: "0",
+      stakingBalance: "0",
+      loading: true
+    }
+  }
+
+  render() {
+    let content
+    {this.state.loading ? content = <p id='loader' className='text-center' style={{margin:"30px"}}>Loading Please...</p> : content =<Main
+    tetherBalance ={this.state.tetherBalance}
+    rwdBalance ={this.state.rwdBalance}
+    stakingBalance ={this.state.stakingBalance}
+    />}
+
+    return (
+      <div>
+        <Navbar account={this.state.account} />
+        <div className='container-fluid mt-5' >
+          <div className='row'>
+            <main role='main' className='col-lg-12 ml-auto mr-auto' style={{ maxWidth: "600px", minHeight: "100vm" }}>
+              <div>
+                {content}
+              </div>
+
+            </main>
+          </div>
+
+        </div>
+      </div>
+    )
+
+  }
+}
+
+export default App
