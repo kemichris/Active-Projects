ecc538626f40ee6299d3aeb6d35b9f0aa6721898
kemi Chris <ekeminichristopher123@gmail.com>
2024-03-04 13:06:06 +0100

    new app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>
+        <nav>
+            <img src="" alt="" />
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">About</a>
+                </li>
+            </ul>
+
+        </nav>
+    </div>
+  )
+}
