457ab633926eeec22a10e6db7746f52097e55c6b
kemi Chris <ekeminichristopher123@gmail.com>
2024-03-08 11:43:04 +0100

    contract integration

--- src/components/ProposalsSection.jsx
+++ src/components/ProposalsSection.jsx
@@ -1,17 +1,29 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 // contract
 import ContractAbi from "../contract/contractAbi.json";
 import { ethers } from 'ethers';
-import { SnackbarProvider, enqueueSnackbar } from 'notistack';
+// import { SnackbarProvider, enqueueSnackbar } from 'notistack';
+import { wait } from '@testing-library/user-event/dist/utils';
+// import { errors } from 'web3';
 
 
 
 export const ProposalsSection = () => {
 
-    const contractAddress = "0x48564fc514e458861BD7891Df840A37dB78386f2"
+    const contractAddress = "0x48564fc514e458861BD7891Df840A37dB78386f2";
 
-    const winningProposal = async ()=> {
+    const [value, setValue] = useState("");
+
+
+    const handleInput = (event) => {
+        const inputValue = event.target.checked ? parseInt(event.target.value, 10) : "";
+        setValue(inputValue);
+        console.log(inputValue);
+    }
+
+    //Checking the winning proposal
+    /*const winningProposal = async () => {
         try {
             const provider = new ethers.BrowserProvider(window.ethereum);
             await provider.send("eth_requestAccounts", []);
@@ -20,52 +32,97 @@ export const ProposalsSection = () => {
 
             const winningName = votingContract.winningName();
 
-            enqueueSnackbar(winningName + "project is leading", {
-                variant: "success"
-            } );
+            // enqueueSnackbar(winningName + "project is leading", {
+            //     variant: "success"
+            // });
+
+            console.log(winningName)
 
         } catch (error) {
             console.log("Error Message:", error.data)
         }
+    }*/
+
+    const voteProposal = async (event) => {
+        event.preventDefault();
+
+        try {
+            const provider = new ethers.BrowserProvider(window.ethereum)
+            await provider.send("eth_requestAccounts", []);
+            const signer = await provider.getSigner();
+            // const address = await signer.getAddress();
+            let receipt;
+            const votingContract = new ethers.Contract(contractAddress, ContractAbi, signer);
+
+            console.log(receipt)
+            // const voterInfo = await votingContract.voters[address];
+            // const hasVoted = voterInfo.voted;
+            // console.log(hasVoted)
+
+            // if (hasVoted) {
+            //     console.log("Already Voted")
+
+            // } else {
+            //     //process the vote
+            //     console.log("voting...")
+
+            // }
+
+            //proceed with voting
+            const transaction = await votingContract.voteProposal(parseInt(value, 10));
+
+            receipt = await wait(transaction);
+            console.log("vote submitted successfully");
+            // enqueueSnackbar("Vote Successful", {variant: "success"});
+        } catch (error) {
+            // enqueueSnackbar(error.data.message, {variant: "error"});
+            console.log("Failed, reason", error)
+        }
+
     }
-    
-  return (
-    <div className='proposal-section'>
-        <h2>Proposals</h2>
-        <p className='proposal-p'>Here are the proposals submitted by the community; every member has the right to vote for their choice of proposals.</p>
-        <div className="proposal-card">
-            <div className="proposal-1">
-                <h3>Community Events Sponsorship</h3>
-                <p className='txt-white'>Proposal to allocate funds for sponsoring local community events related to blockchain and technology.</p>
-                <p className='vote-p'>Vote count <span>0</span></p>
-            </div>
-            <div className="proposal-2">
-                <h3>Development Grant Program</h3>
-                <p className='txt-white'>Proposal to establish a grant program to support developers working on innovative blockchain projects.</p>
-                <p className='vote-p'>Vote count <span>0</span></p>
-            </div>
-            <div className="proposal-3">
-                <h3>Educational Webinar Series</h3>
-                <p className='txt-white'>Proposal to organize a series of educational webinars to increase awareness and understanding of blockchain technology.</p>
-                <p className='vote-p'>Vote count <span>0</span></p>
-            </div>
-            <div className="proposal-4">
-                <h3>Blockchain for Social Impact</h3>
-                <p className='txt-white'>PProposal to allocate resources for blockchain projects aimed at addressing social and environmental challenges.</p>
-                <p className='vote-p'>Vote count <span>0</span></p>
-            </div>
-            <div className="proposal-5">
-                <h3>Security and Auditing Framework</h3>
-                <p className='txt-white'>Proposal to allocate resources for blockchain projects aimed at addressing social and environmental challenges.</p>
-                <p className='vote-p'>Vote count <span>0</span></p>
-            </div>
+    return (
+
+        <div className='proposal-section'>
+            <h2>Proposals</h2>
+            <p className='proposal-p'>Here are the proposals submitted by the community; every member has the right to vote for their choice of proposals.</p>
+            <form onSubmit={voteProposal}>
+                <div className="proposal-card">
+                    <input type="radio" name="option" value="0" id="option1" onChange={handleInput} />
+                    <label htmlFor='option1' className="proposal-1">
+                        <h3>Community Events Sponsorship</h3>
+                        <p className='txt-white'>Proposal to allocate funds for sponsoring local community events related to blockchain and technology.</p>
+                        <p className='vote-p'>Vote count <span>0</span></p>
+                    </label>
+                    <input type="radio" name="option" value="1" id="option2" onChange={handleInput} />
+                    <label htmlFor='option2' className="proposal-2">
+                        <h3>Development Grant Program</h3>
+                        <p className='txt-white'>Proposal to establish a grant program to support developers working on innovative blockchain projects.</p>
+                        <p className='vote-p'>Vote count <span>0</span></p>
+                    </label>
+                    <input type="radio" name="option" value="2" id="option3" onChange={handleInput} />
+                    <label htmlFor='option3' className="proposal-3">
+                        <h3>Educational Webinar Series</h3>
+                        <p className='txt-white'>Proposal to organize a series of educational webinars to increase awareness and understanding of blockchain technology.</p>
+                        <p className='vote-p'>Vote count <span>0</span></p>
+                    </label>
+                    <input type="radio" name="option" value="3" id="option4" onChange={handleInput} />
+                    <label htmlFor='option4' className="proposal-4">
+                        <h3>Blockchain for Social Impact</h3>
+                        <p className='txt-white'>PProposal to allocate resources for blockchain projects aimed at addressing social and environmental challenges.</p>
+                        <p className='vote-p'>Vote count <span>0</span></p>
+                    </label>
+                    <input type="radio" name="option" value="4" id="option5" onChange={handleInput} />
+                    <label htmlFor='option5' className="proposal-5">
+                        <h3>Security and Auditing Framework</h3>
+                        <p className='txt-white'>Proposal to allocate resources for blockchain projects aimed at addressing social and environmental challenges.</p>
+                        <p className='vote-p'>Vote count <span>0</span></p>
+                    </label>
+
+                </div>
+                <button type='submit' className='vote-btn txt-white'>Vote</button>
+            </form>
+
+
         </div>
-        <form action="">
-
-
-        </form>
-
-        <button className='vote-btn txt-white'>Vote</button>
-    </div>
-  )
+    )
 }
--- src/components/SubmitSection.jsx
+++ src/components/SubmitSection.jsx
@@ -1,19 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+// contract
+import ContractAbi from "../contract/contractAbi.json";
+import { ethers } from 'ethers';
 
 export const SubmitSection = () => {
+
+  const contractAddress = "0x48564fc514e458861BD7891Df840A37dB78386f2";
+
+  const [votersAddress, setVotersAddress] = useState('');
+
+  const submitAddress = async (event) => {
+    event.preventDefault();
+    try {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      await provider.send("eth_requestAccounts", []);
+      const signer = await provider.getSigner();
+      // const address = await signer.getAddress();
+      const votingContract = new ethers.Contract(contractAddress, ContractAbi, signer);
+
+      const transaction = await votingContract.submitAddress(votersAddress);
+
+      //you wait for the transaction to be mined
+      let receipt;
+      receipt = await transaction.wait();
+
+      console.log(receipt)
+
+      // Optionally, reset the input field after submission
+      setVotersAddress('');
+
+      console.log(votersAddress + "submited")
+
+
+    } catch (error) {
+      console.error('Error submitting voters address:', error);
+    }
+
+  }
+
+  const handleInputChange = (event) => {
+    setVotersAddress(event.target.value);
+};
+
+
+
   return (
     <div className='submit-container'>
-        <h3 className='primary-color'>Sumbit Address</h3>
-        <p className='txt-white'>Ready to make your voice heard? We invite all interested users to participate in the voting process. 
-            Simply submit your Ethereum Virtual Machine (EVM) address in the space below and hit the submit button.
+      <h3 className='primary-color'>Sumbit Address</h3>
+      <p className='txt-white'>Ready to make your voice heard? We invite all interested users to participate in the voting process.
+        Simply submit your Ethereum Virtual Machine (EVM) address in the space below and hit the submit button.
+      </p>
+      <form onSubmit={submitAddress}>
+        <input type="text" placeholder='voters address' value={votersAddress} onChange={handleInputChange} className='primary-color' required />
+        <p className='txt-white'>By submitting your address, you gain the power to influence the future.
+          Be a part of the decision-making process. submit now!
         </p>
-        <form action="">
-            <input type="text" placeholder='voters address' name="" className='primary-color' required />
-            <p className='txt-white'>By submitting your address, you gain the power to influence the future. 
-                Be a part of the decision-making process. submit now!
-            </p>
-            <button type="submit" className=' submit-btn txt-white'>Submit</button>
-        </form>
+        <button type="submit" className=' submit-btn txt-white'>Submit</button>
+      </form>
     </div>
   )
 }
